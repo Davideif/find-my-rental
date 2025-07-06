ec6c4362092075b648645557da175bb7340231f5
Davideif <dpmcantabria1994@gmail.com>
2025-07-07 00:14:40 +0100

    up to hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Hero from '@/components/Hero.jsx';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Hero/>
   );
 };      
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
 import { FaGoogle } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
 
 
 const Navbar = () => {
+
+  const[isMobileMenuOpen,setIsMobileMenuOpen] = useState(false);
+  const[isProfileMenuOpen,setIsProfileMenuOpen] = useState(false); 
+  const[isLoggedIn,setIsLoggedIn]=useState(false);
+
+  const pathname = usePathname();
+ 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -19,6 +28,7 @@ const Navbar = () => {
           className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           aria-controls="mobile-menu"
           aria-expanded="false"
+          onClick={()=>setIsMobileMenuOpen((prev)=>!prev)}
         >
           <span className="absolute -inset-0.5"></span>
           <span className="sr-only">Open main menu</span>
@@ -57,39 +67,42 @@ const Navbar = () => {
           <div className="flex space-x-2">
             <Link
               href="/"
-              className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+              className={`${pathname === '/' ? 'bg-black' : ''} text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`} 
             >
               Home
             </Link>
             <Link
               href="/properties"
-              className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+              className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
             >
               Properties
             </Link>
+            {isLoggedIn && (
             <Link
-              href="/add-property.html"
-              className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+              href="/properties/add"
+              className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
             >
               Add Property
-            </Link>
+            </Link>)}
           </div>
         </div>
       </div>
 
       {/* Right Side Menu (Logged Out) */}
-      <div className="hidden md:block md:ml-6">
+      {!isLoggedIn && (<div className="hidden md:block md:ml-6">
         <div className="flex items-center">
           <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
             <FaGoogle  className="text-white mr-2" />
             <span>Login or Register</span>
           </button>
         </div>
-      </div>
+      </div>)}
+      
 
       {/* Right Side Menu (Logged In) */}
       <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-        <Link href="messages.html" className="relative group">
+          {isLoggedIn && (
+         <Link href="messages.html" className="relative group">
           <button
             type="button"
             className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -117,8 +130,11 @@ const Navbar = () => {
           </span>
         </Link>
 
+      )}
+       
         {/* Profile dropdown button */}
         <div className="relative ml-3">
+        {isLoggedIn && (
           <div>
             <button
               type="button"
@@ -126,6 +142,7 @@ const Navbar = () => {
               id="user-menu-button"
               aria-expanded="false"
               aria-haspopup="true"
+              onClick={()=>setIsProfileMenuOpen((prev)=>!prev)}
             >
               <span className="absolute -inset-1.5"></span>
               <span className="sr-only">Open user menu</span>
@@ -134,18 +151,22 @@ const Navbar = () => {
                 src={profileDefault}
                 alt=""
               />
+          
             </button>
           </div>
+        )}
+        
+          
 
           {/* Profile dropdown */}
+          {isProfileMenuOpen && (
           <div
             id="user-menu"
-            className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="user-menu-button"
-            tabIndex="-1"
-          >
+            tabIndex="-1">
             <Link
               href="/profile"
               className="block px-4 py-2 text-sm text-gray-700"
@@ -172,14 +193,14 @@ const Navbar = () => {
             >
               Sign Out
             </button>
-          </div>
+          </div>)}
         </div>
       </div>
     </div>
   </div>
 
   {/* Mobile menu, show/hide based on menu state. */}
-  <div className="hidden" id="mobile-menu">
+  {isMobileMenuOpen && (<div  id="mobile-menu">
     <div className="space-y-1 px-2 pb-3 pt-2">
       <Link
         href="/"
@@ -193,18 +214,24 @@ const Navbar = () => {
       >
         Properties
       </Link>
-      <Link
-        href="/add-property"
+      {isLoggedIn &&( 
+        <Link
+        href="/properties/add"
         className="text-white block rounded-md px-3 py-2 text-base font-medium"
       >
         Add Property
       </Link>
-      <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
+
+      )}
+    
+      {!isLoggedIn && ( <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
         <FaGoogle  className="text-white mr-2"/>
         <span>Login or Register</span>
-      </button>
+      </button>)}
+     
     </div>
-  </div>
+  </div>)}
+  
 </nav>
 
   );
